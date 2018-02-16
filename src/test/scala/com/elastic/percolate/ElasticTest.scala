e9d88dd3eada3b223a67634d561b1dde41015da6
Gerard Vico <gerard.vico@gmail.com>
2018-02-16 11:54:31 +0100

    Index a document example

--- src/test/scala/com/elastic/percolate/ElasticTest.scala
+++ src/test/scala/com/elastic/percolate/ElasticTest.scala
@@ -1,8 +1,13 @@
 package com.elastic.percolate
 
+import java.util.UUID
+
 import org.apache.http.HttpHost
+import org.elasticsearch.action.DocWriteResponse
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest
+import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.{RestClient, RestHighLevelClient}
+import org.elasticsearch.common.xcontent.XContentFactory
 
 import scala.util.Try
 
@@ -23,6 +28,23 @@ object TestElasticSearchPercolate {
       val requestCreateIndex = new CreateIndexRequest("letxchange")
       restClient.indices().create(requestCreateIndex)
     }.fold(_ => println(s"Index $indexNameToCreate already exist!"), _ => println(s"Index $indexNameToCreate created!"))
+
+    // Index a document
+    val productId = UUID.randomUUID().toString
+
+    val builder = XContentFactory.jsonBuilder
+    builder.startObject
+    builder.field("product-id", productId)
+    builder.array("key-words", "iphone", "apple")
+    builder.array("tradable-keywords", "samsung", "X5")
+    builder.endObject
+
+    val indexRequest = new IndexRequest("letxchange", "products", productId).source(builder)
+    if (restClient.index(indexRequest).getResult == DocWriteResponse.Result.CREATED) {
+      println(s"Document indexed: ${builder.toString}")
+    } else {
+      println(s"Error indexing document: ${builder.toString}")
+    }
 
 
     // close client
