647f528c24c7a494f1b4c167649f0fa6e5a5a46a
Gerard Vico <gerard.vico@gmail.com>
2018-02-16 11:31:16 +0100

    Clean the code and add a check to handle if the index alread exist

--- src/test/scala/com/elastic/percolate/ElasticTest.scala
+++ src/test/scala/com/elastic/percolate/ElasticTest.scala
@@ -1,12 +1,10 @@
 package com.elastic.percolate
 
-import java.util.UUID
-
 import org.apache.http.HttpHost
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest
-import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.client.{RestClient, RestHighLevelClient}
-import org.elasticsearch.common.xcontent.XContentFactory._
+
+import scala.util.Try
 
 
 object TestElasticSearchPercolate {
@@ -19,47 +17,16 @@ object TestElasticSearchPercolate {
 
 
     // create an index
-   // val requestCreateIndex = new CreateIndexRequest("letxchange")
-   // restClient.indices().create(requestCreateIndex)
+    val indexNameToCreate = "letxchange"
 
-    // indexing a document
-    val productId = UUID.randomUUID()
-
-    jsonBuilder.startObject("tradable_product")
-    jsonBuilder.array("key_words", "apple", "iphone 6")
-    jsonBuilder.array("trade_keywords", "Samsung", "S5")
-    jsonBuilder.endObject
-
-    val indexRequest = new IndexRequest("letxchange", "producto", productId.toString).source(jsonBuilder)
-    restClient.index(indexRequest)
+    Try {
+      val requestCreateIndex = new CreateIndexRequest("letxchange")
+      restClient.indices().create(requestCreateIndex)
+    }.fold(_ => println(s"Index $indexNameToCreate already exist!"), _ => println(s"Index $indexNameToCreate created!"))
 
 
-    /*
-    val productUUID = UUID.randomUUID()
-
-    val response = client.prepareIndex("productos", "producto")
-      .setSource(jsonBuilder()
-        .startObject()
-        .field("uuid", productUUID.toString)
-        .field("owner", "Gerard Vico")
-        .field("location", "Spain")
-        .array("keywords", "iphone", "apple", "plus")
-        .array("trade_keywords", "samsung", "S5")
-        .endObject()
-      )
-      .get()
-      */
-
-
-    // Search by criteria
-    /* val searchResponse = client.prepareSearch("casas")
-      .setTypes("tiny_house")
-      .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-      .setQuery(QueryBuilders.termQuery("multi", "test"))                 // Query
-      .setPostFilter(QueryBuilders.rangeQuery("age").from(12).to(18))     // Filter
-      .setFrom(0).setSize(60).setExplain(true)
-      .get() */
-
+    // close client
+    restClient.close()
   }
 
 }
